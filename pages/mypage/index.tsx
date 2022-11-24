1280097baa320cadaebc1f03ce1f628a092cc6eb
JongGeun <dldvk9999@naver.com>
2022-11-24 15:30:20 +0900

    Header - router page update

---
+++ pages/mypage/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.scss";
+
+export default function Mypage() {
+    return (
+        <main>
+            <div>This is Mypage page.</div>
+        </main>
+    );
+}
