8a8b8343ff7f67f530344df32996ecafe6203e00
JongGeun <dldvk9999@naver.com>
2022-11-28 19:49:43 +0900

    Menu - init setting

--- data/store.tsx
+++ data/store.tsx
@@ -6,6 +6,67 @@ const store = [
         date: "2022.10.25",
         locate: "서울시 어쩌구 저쩌구",
         category: ["FRIED", "SNACK", "RICE", "BEVERAGE", "DRINK"],
+        menu: {
+            FRIED: [
+                {
+                    name: "새우 튀김",
+                    price: 15000,
+                    desc: "독도 새우로 만든 새우 튀김! 값만큼 맛도 만점!",
+                    image: "새우튀김.jpg",
+                    soldout: false,
+                },
+                {
+                    name: "오징어 튀김",
+                    price: 10000,
+                    desc: "엄청난 양으로 4명이서 먹어도 충분한 튀김!",
+                    image: "오징어튀김.jpg",
+                    soldout: false,
+                },
+            ],
+            SNACK: [
+                {
+                    name: "오레오",
+                    price: 2000,
+                    desc: "다른데가 너무 비싸 제가 직접 싸게 만들었습니다.",
+                    image: "오레오.png",
+                    soldout: true,
+                },
+            ],
+            RICE: [
+                {
+                    name: "공기밥",
+                    price: 1000,
+                    desc: "일반 공기밥",
+                    image: "공기밥.jpg",
+                    soldout: false,
+                },
+            ],
+            BEVERAGE: [
+                {
+                    name: "하리보",
+                    price: 2000,
+                    desc: "이것도 다른데가 너무 비싸 제가 직접 싸게 만들었습니다.",
+                    image: "하리보.jpg",
+                    soldout: false,
+                },
+                {
+                    name: "하리보2",
+                    price: 2000,
+                    desc: "하리보 버전 2 입니다. 가격은 똑같습니다.",
+                    image: "하리보2.jpg",
+                    soldout: true,
+                },
+            ],
+            DRINK: [
+                {
+                    name: "물",
+                    price: 0,
+                    desc: "물은 무료이지만 셀프입니다.",
+                    image: "물.jpg",
+                    soldout: false,
+                },
+            ],
+        },
     },
     {
         id: 2,
@@ -14,6 +75,48 @@ const store = [
         date: "2022.10.26",
         locate: "경기도 어쩌구 저쩌구",
         category: ["치킨", "피자", "음료"],
+        menu: {
+            치킨: [
+                {
+                    name: "황금 올리브 치킨",
+                    price: 20000,
+                    desc: "겉은 바삭 육즙 가득한 부드러운 속살이 환상적인 건강한 치킨, 비비큐의 시그니처 메뉴 후라이드의 대명사 황금올리브치킨",
+                    image: "황금_올리브_치킨.png",
+                    soldout: false,
+                },
+                {
+                    name: "자메이카 소떡만나치킨",
+                    price: 24000,
+                    desc: "BBQ 자메이카 저크소스와 신선육, 소떡소떡을 조합하여 풍미를 올린 전국민이 최애하는 바베큐치킨",
+                    image: "자메이카_소떡만나치킨.png",
+                    soldout: true,
+                },
+            ],
+            피자: [
+                {
+                    name: "돈마호크",
+                    price: 35900,
+                    desc: "돈마호크 180g과 리코타 치즈의 환상 케미",
+                    image: "돈마호크.png",
+                    soldout: false,
+                },
+                {
+                    name: "케이준 더블쉬림프",
+                    price: 35900,
+                    desc: "통새우 30마리를 가득 채운 리얼 프리미엄!",
+                    image: "케이준_더블쉬림프.png",
+                    soldout: false,
+                },
+                {
+                    name: "립스테이크",
+                    price: 35900,
+                    desc: "180g 갈비맛 스테이크가 통째로!",
+                    image: "립스테이크.png",
+                    soldout: false,
+                },
+            ],
+            음료: [],
+        },
     },
     {
         id: 3,
@@ -22,6 +125,60 @@ const store = [
         date: "2022.11.1",
         locate: "인천시 어쩌구 저쩌구",
         category: ["탕류", "찌개류", "전골류", "공기밥", "음료"],
+        menu: {
+            탕류: [
+                {
+                    name: "꽃게탕",
+                    price: 20000,
+                    desc: "백종원 꽃게탕",
+                    image: "꽃게탕.jpg",
+                    soldout: false,
+                },
+                {
+                    name: "매운탕",
+                    price: 10000,
+                    desc: "우럭, 광어 머리가 들어간 얼큰한 매운탕!",
+                    image: "매운탕.jpg",
+                    soldout: false,
+                },
+            ],
+            찌개류: [
+                {
+                    name: "부대찌개",
+                    price: 12000,
+                    desc: "백종원 부대찌개",
+                    image: "부대찌개.jpg",
+                    soldout: true,
+                },
+            ],
+            전골류: [
+                {
+                    name: "곱창전골",
+                    price: 15000,
+                    desc: "백종원 곱창전골",
+                    image: "곱창전골.jpg",
+                    soldout: false,
+                },
+            ],
+            공기밥: [
+                {
+                    name: "공기밥",
+                    price: 1000,
+                    desc: "일반 공기밥",
+                    image: "공기밥.jpg",
+                    soldout: false,
+                },
+            ],
+            음료: [
+                {
+                    name: "물",
+                    price: 0,
+                    desc: "물은 무료이지만 셀프입니다.",
+                    image: "물.jpg",
+                    soldout: true,
+                },
+            ],
+        },
     },
     {
         id: 4,
@@ -30,6 +187,11 @@ const store = [
         date: "2022.11.3",
         locate: "제주도 공항 근처",
         category: ["메인요리", "서브요리", "음료"],
+        menu: {
+            메인요리: [],
+            서브요리: [],
+            음료: [],
+        },
     },
     {
         id: 5,
@@ -38,6 +200,10 @@ const store = [
         date: "2022.11.11",
         locate: "독도",
         category: ["CHICKEN", "PIZZA"],
+        menu: {
+            CHICKEN: [],
+            PIZZA: [],
+        },
     },
     {
         id: 6,
@@ -46,6 +212,11 @@ const store = [
         date: "2022.11.19",
         locate: "연평도 가운데",
         category: ["FISH", "PASTA", "RICE"],
+        menu: {
+            FISH: [],
+            PASTA: [],
+            RICE: [],
+        },
     },
     {
         id: 7,
@@ -54,6 +225,11 @@ const store = [
         date: "2022.11.22",
         locate: "여긴 어디로 할까",
         category: ["what", "to", "sell"],
+        menu: {
+            what: [],
+            to: [],
+            sell: [],
+        },
     },
 ];
 
--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,10 +1,320 @@
+import Image from "next/image";
+import store from "../../data/store";
 import LoginCheck from "../login_check";
 import styles from "../../styles/pages/Menu.module.scss";
+import { useState } from "react";
+
+type menuList = {
+    name: string;
+    price: number;
+    desc: string;
+    image: string;
+    soldout: boolean;
+};
+type menu = {
+    [key: string]: Array<Object>;
+};
 
 export default function Menu() {
+    const [storeID, setStoreID] = useState(-1);
+    const [category, setCategory] = useState("");
+    const [itemName, setItemName] = useState("");
+    const [itemPrice, setItemPrice] = useState(0);
+    const [itemDesc, setItemDesc] = useState("");
+    const [itemImage, setItemImage] = useState<any>();
+
+    // 왼쪽에 선택한 아이템이나 입력한 아이템 모두 리셋
+    function editReset() {
+        setCategory("");
+        setItemName("");
+        setItemPrice(0);
+        setItemDesc("");
+        setItemImage("");
+    }
+
+    // 수정 버튼 클릭 시 해당 아이템 정보 가져옴
+    function menuEdit(
+        categoryIndex: string,
+        name: string,
+        price: number,
+        desc: string,
+        image: string
+    ) {
+        setCategory(categoryIndex);
+        setItemName(name);
+        setItemPrice(price);
+        setItemDesc(desc);
+        setItemImage("/sample_menu/" + image);
+    }
+
+    // 매장에 있는 메뉴 - 세부 아이템 출력
+    function printStoreMenuListItem(category: string, index: number) {
+        let result = [];
+        if (storeID !== -1) {
+            const menu = store[storeID - 1].menu as unknown as menu;
+            if (menu[category].length) {
+                let tmp = Array.from(
+                    { length: menu[category].length },
+                    () => false
+                );
+                for (let i = 0; i < menu[category].length; i++) {
+                    tmp[i] = (menu[category][i] as menuList).soldout;
+                    result.push(
+                        <div className={styles.menuItem} key={"menu-list-" + i}>
+                            <Image
+                                src={
+                                    "/sample_menu/" +
+                                    (menu[category][i] as menuList).image
+                                }
+                                alt={(menu[category][i] as menuList).name}
+                                width={75}
+                                height={75}
+                                className={styles.menuItemImage}
+                                priority
+                            />
+                            <div className={styles.menuItemNamePrice}>
+                                <p>
+                                    {(menu[category][i] as menuList).name}
+                                    {(menu[category][i] as menuList)
+                                        .soldout && (
+                                        <span
+                                            className={styles.menuItemSoldout}
+                                        >
+                                            품절
+                                        </span>
+                                    )}
+                                </p>
+                                <p>{(menu[category][i] as menuList).desc}</p>
+                                <p>{(menu[category][i] as menuList).price}원</p>
+                            </div>
+                            <button
+                                className={styles.menuItemBtn}
+                                onClick={() =>
+                                    menuEdit(
+                                        category,
+                                        (menu[category][i] as menuList).name,
+                                        (menu[category][i] as menuList).price,
+                                        (menu[category][i] as menuList).desc,
+                                        (menu[category][i] as menuList).image
+                                    )
+                                }
+                            >
+                                수정
+                            </button>
+                            <button
+                                className={styles.menuItemBtn}
+                                onClick={() => {
+                                    if (confirm("삭제하시겠습니까?"))
+                                        alert("삭제 완료하였습니다.");
+                                }}
+                            >
+                                삭제
+                            </button>
+                            <button
+                                className={styles.menuItemBtn}
+                                onClick={() => {
+                                    alert("품절처리 완료하였습니다.");
+                                }}
+                            >
+                                품절
+                            </button>
+                        </div>
+                    );
+                }
+            } else {
+                result.push(
+                    <div
+                        className={styles.menuItemNoExist}
+                        key={"menu-list-no-exist"}
+                    >
+                        상품이 없습니다.
+                    </div>
+                );
+            }
+        }
+        return result;
+    }
+
+    // 매장에 있는 메뉴 목록 출력
+    function printStoreMenuList() {
+        let result = [];
+        if (storeID !== -1) {
+            for (let i = 0; i < store[storeID - 1].category.length; i++) {
+                result.push(
+                    <div
+                        className={styles.menuItemHeader}
+                        key={"menu-list-header-" + i}
+                    >
+                        <h3>{store[storeID - 1].category[i]}</h3>
+                        {printStoreMenuListItem(
+                            store[storeID - 1].category[i],
+                            i
+                        )}
+                    </div>
+                );
+            }
+        }
+        return result;
+    }
+
+    // 선택한 매장 ID의 카테고리를 드롭다운 선택
+    function printStoreCategory() {
+        let result = [];
+        result.push(
+            <option value={""} hidden key={"menu-storeCategory-default"}>
+                카테고리를 선택해주세요.
+            </option>
+        );
+        if (storeID !== -1) {
+            for (let i = 0; i < store[storeID - 1].category.length; i++) {
+                result.push(
+                    <option
+                        value={store[storeID - 1].category[i]}
+                        key={"menu-storeCategory-" + i}
+                    >
+                        {i + 1}. {store[storeID - 1].category[i]}
+                    </option>
+                );
+            }
+        }
+        return result;
+    }
+
+    // 매장 ID 검색 후 드롭다운 선택
+    function printStoreID() {
+        let result = [];
+        result.push(
+            <option value={-1} hidden key={"menu-storeID-default"}>
+                매장 ID를 선택해주세요.
+            </option>
+        );
+        for (let i = 0; i < store.length; i++) {
+            result.push(
+                <option value={store[i].id} key={"menu-storeID-" + i}>
+                    {store[i].id}. {store[i].name}
+                </option>
+            );
+        }
+        return result;
+    }
+
+    // 이미지 파일 업로드 시 미리보기 함수
+    function readImage(input: any) {
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setItemImage(e.target!.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        } else {
+            setItemImage("");
+        }
+    }
+
     return LoginCheck() ? (
         <main>
-            <div>This is Menu page.</div>
+            <div className={styles.menu}>
+                <section className={styles.menuInfo}>
+                    <h1>
+                        메뉴 등록
+                        <span>
+                            <button onClick={editReset}>
+                                <Image
+                                    src={"/reload.webp"}
+                                    alt={"reload"}
+                                    width={25}
+                                    height={25}
+                                />
+                            </button>
+                        </span>
+                    </h1>
+                    <div className={styles.menuInputImageP}>
+                        <Image
+                            id="preview-image"
+                            src={
+                                itemImage
+                                    ? itemImage
+                                    : "/sample_menu/이미지추가.png"
+                            }
+                            alt={"preview image"}
+                            width={150}
+                            height={150}
+                            className={`${styles.menuInputImage} ${
+                                !itemImage && styles.menuInputHide
+                            }`}
+                            priority
+                        />
+                        <div className={styles.menuInputImageInput}>
+                            <input
+                                id="files"
+                                type="file"
+                                accept="image/*"
+                                onChange={(e) => readImage(e.target)}
+                                className={`${styles.menuInputImage} ${styles.menuInputHideDisplay}`}
+                            />
+                            <label
+                                htmlFor="files"
+                                className={`${styles.menuInputImage} ${
+                                    itemImage && styles.menuInputHide
+                                }`}
+                            ></label>
+                            <p
+                                className={
+                                    itemImage && styles.menuInputHideDisplay
+                                }
+                            >
+                                이미지 추가
+                            </p>
+                        </div>
+                    </div>
+                    <select
+                        value={storeID}
+                        onChange={(e) => setStoreID(Number(e.target.value))}
+                        className={`${styles.menuInput} ${styles.menuInputSelect}`}
+                    >
+                        {printStoreID()}
+                    </select>
+                    <select
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        className={`${styles.menuInput} ${styles.menuInputSelect}`}
+                        disabled={storeID === -1}
+                    >
+                        {printStoreCategory()}
+                    </select>
+                    <input
+                        type="text"
+                        placeholder="이름"
+                        onChange={(e) => setItemName(e.target.value)}
+                        value={itemName}
+                        disabled={storeID === -1 || category === ""}
+                        className={styles.menuInput}
+                    />
+                    <input
+                        type="number"
+                        placeholder="가격"
+                        onChange={(e) => setItemPrice(Number(e.target.value))}
+                        value={itemPrice}
+                        disabled={storeID === -1 || category === ""}
+                        className={styles.menuInput}
+                    />
+                    <textarea
+                        placeholder="설명"
+                        onChange={(e) => setItemDesc(e.target.value)}
+                        value={itemDesc}
+                        disabled={storeID === -1 || category === ""}
+                        className={`${styles.menuInput} ${styles.menuInputTextarea}`}
+                    />
+                    <button className={styles.menuAddBtn}>메뉴 추가</button>
+                </section>
+
+                <section className={styles.menuList}>
+                    <h2>메뉴 리스트</h2>
+                    <div className={styles.menuItemCategory}>
+                        <div>{printStoreMenuList()}</div>
+                    </div>
+                </section>
+            </div>
         </main>
     ) : null;
 }
