0df17e2d4d866aa33765e974045caf5e5a300f7a
JongGeun <dldvk9999@naver.com>
2022-11-30 13:36:55 +0900

    Signup - layout update

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -7,6 +7,7 @@ export default function Signup() {
     const [email, setEmail] = useState("");
     const [pass1, setPass1] = useState("");
     const [pass2, setPass2] = useState("");
+    const [signupOK, setSignupOK] = useState(false);
 
     function signup(e: { preventDefault: () => void }) {
         if (email === "" || pass1 === "" || pass2 === "") {
@@ -19,7 +20,7 @@ export default function Signup() {
             alert("비밀번호는 최소 8자 이내로 해주세요.");
             e.preventDefault();
         } else {
-            alert("signup function complete");
+            setSignupOK(true);
         }
     }
 
@@ -32,30 +33,40 @@ export default function Signup() {
                     width={150}
                     height={150}
                 />
-                <form
-                    className={styles.loginForm}
-                    onSubmit={signup}
-                    action="/login"
-                >
-                    <input
-                        type="email"
-                        placeholder="이메일"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="비밀번호"
-                        minLength={8}
-                        onChange={(e) => setPass1(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="비밀번호 확인"
-                        minLength={8}
-                        onChange={(e) => setPass2(e.target.value)}
-                    />
-                    <button type="submit">회원가입</button>
-                </form>
+                {!signupOK ? (
+                    <form
+                        className={styles.loginForm}
+                        onSubmit={signup}
+                        action="/login"
+                    >
+                        <input
+                            type="email"
+                            placeholder="이메일"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="비밀번호"
+                            minLength={8}
+                            onChange={(e) => setPass1(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="비밀번호 확인"
+                            minLength={8}
+                            onChange={(e) => setPass2(e.target.value)}
+                        />
+                        <button type="submit">회원가입</button>
+                    </form>
+                ) : (
+                    <>
+                        <p>회원가입이 완료되었습니다.</p>
+                        <p>이메일 인증을 완료하신 후 로그인하여 주세요.</p>
+                        <Link href={"/login"}>
+                            <button>로그인</button>
+                        </Link>
+                    </>
+                )}
             </section>
         </main>
     );
