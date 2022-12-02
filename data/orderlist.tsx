4c7303d609fac36ec401b9f28826950aee2d2d69
JongGeun <dldvk9999@naver.com>
2022-12-02 23:37:21 +0900

    OrderList - design update

---
+++ data/orderlist.tsx
@@ -0,0 +1,68 @@
+const orderlist = [
+    {
+        name: "진짜 맛있는 집",
+        number: 2,
+        total: 250000,
+        time: "2022.12.2 15:02",
+        menu: [
+            {
+                name: "치킨",
+                price: 20000,
+                count: 10,
+            },
+            {
+                name: "피자",
+                price: 25000,
+                count: 2,
+            },
+        ],
+    },
+    {
+        name: "진짜 맛있는 집",
+        number: 14,
+        total: 12000,
+        time: "2022.12.2 15:31",
+        menu: [
+            {
+                name: "오징어튀김",
+                price: 2000,
+                count: 4,
+            },
+            {
+                name: "새우튀김",
+                price: 2000,
+                count: 2,
+            },
+        ],
+    },
+    {
+        name: "진짜 맛있는 집",
+        number: 9,
+        total: 60000,
+        time: "2022.12.2 16:59",
+        menu: [
+            {
+                name: "부대찌개",
+                price: 20000,
+                count: 2,
+            },
+            {
+                name: "곱창전골",
+                price: 10000,
+                count: 1,
+            },
+            {
+                name: "공기밥",
+                price: 1000,
+                count: 6,
+            },
+            {
+                name: "음료",
+                price: 2000,
+                count: 2,
+            },
+        ],
+    },
+];
+
+export default orderlist;
--- pages/orderlist/index.tsx
+++ pages/orderlist/index.tsx
@@ -1,11 +1,84 @@
+import { useEffect, useState } from "react";
+import orderlist from "../../data/orderlist";
 import LoginCheck from "../login_check";
 import styles from "../../styles/pages/OrderList.module.scss";
 
+type menu = {
+    name: string;
+    price: number;
+    count: number;
+};
+
 export default function OrderList() {
+    const [myOrderList, setOrderList] = useState<any>([]);
+    const [showTableRow, setTableRow] = useState<Array<boolean>>([]);
+
+    // Row를 클릭했을 때 해당 Row 펼치기
+    function showRow(index: number) {
+        setTableRow(
+            showTableRow
+                .slice(0, index)
+                .concat(!showTableRow[index])
+                .concat(showTableRow.slice(index + 1, showTableRow.length))
+        );
+    }
+
+    // 주문 목록 - 메뉴 출력
+    function printOrderListMenu(menu: Array<menu>) {
+        let result = [];
+        for (let i = 0; i < menu.length; i++) {
+            result.push(
+                <p key={"orderlist-menu-item-" + i}>
+                    {menu[i].name} * {menu[i].count} = {(menu[i].price * menu[i].count).toLocaleString()}원
+                </p>
+            );
+        }
+        return result;
+    }
+
+    // 주문 목록 출력
+    function printOrderList() {
+        let result = [];
+        for (let i = 0; i < myOrderList.length; i++) {
+            result.push(
+                <div
+                    className={`${styles.orderlistGridBody} ${showTableRow[i] && styles.orderlistGridShow}`}
+                    onClick={() => showRow(i)}
+                    key={"orderlist-menu-" + i}
+                >
+                    <p>{myOrderList[i].name}</p>
+                    <p>{myOrderList[i].number}번</p>
+                    <p>{myOrderList[i].total.toLocaleString()}원</p>
+                    <p>{myOrderList[i].time}</p>
+                    <p className={styles.orderlistGridBlank}></p>
+                    <p className={styles.orderlistGridBlank}></p>
+                    <div>{printOrderListMenu(myOrderList[i].menu)}</div>
+                    <p className={styles.orderlistGridBlank}></p>
+                </div>
+            );
+        }
+        return result;
+    }
+
+    useEffect(() => {
+        setOrderList(orderlist);
+        setTableRow(Array.from({ length: orderlist.length }, () => false));
+    }, []);
+
     return LoginCheck() ? (
         <main>
-            <section>
-                <div>This is OrderList page.</div>
+            <section className={styles.orderlist}>
+                <h1>주문 내역 조회</h1>
+                <div className={styles.orderlistGrid}>
+                    <h2>매장 이름</h2>
+                    <h2>
+                        테이블{"("}주문{")"} 번호
+                    </h2>
+                    <h2>총합</h2>
+                    <h2>주문 시간</h2>
+                </div>
+
+                {printOrderList()}
             </section>
         </main>
     ) : null;
