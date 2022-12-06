27b305279c8c1850c154d9f18ca259ba133c7046
JongGeun <dldvk9999@naver.com>
2022-12-06 17:15:26 +0900

    Review - init setting

---
+++ data/reviews.tsx
@@ -0,0 +1,77 @@
+const reviews = [
+    {
+        id: 1,
+        review: [
+            {
+                name: "익명1",
+                time: "2022.12.5 12:31",
+                comment: "이 가게 너무 좋습니다! 또 오고싶어요!",
+                profile: "",
+                reply: "감사합니다 익명1님!"
+            },
+            {
+                name: "익명2",
+                time: "2022.12.5 12:38",
+                comment: "사장님이 되게 친절하시네요 ㅎㅎ",
+                profile: "",
+                reply: ""
+            },
+            {
+                name: "익명3",
+                time: "2022.12.5 12:48",
+                comment: "가게 위생이 조금 별로였습니다...",
+                profile: "",
+                reply: "죄송합니다. 신경 쓰도록 하겠습니다 ㅠㅠ"
+            },
+        ]
+    },
+    {
+        id: 2,
+        review: [
+            {
+                name: "익명4",
+                time: "2022.12.4 13:01",
+                comment: "사징님 이 가게 너무 좋습니다. 감사합니다.",
+                profile: "",
+                reply: ""
+            },
+            {
+                name: "익명5",
+                time: "2022.12.6 15:11",
+                comment: "쌇짢늼이 륍뷿갛욨핷엏욝 ㅎㅎ 뼗롵엙욛.",
+                profile: "",
+                reply: "? 뭐라는겨;"
+            },
+        ]
+    },
+    {
+        id: 3,
+        review: [
+            {
+                name: "익명6",
+                time: "2022.12.5 09:00",
+                comment: "그럭저럭인 것 같습니다. 음식 맛은 괜찮았어요~",
+                profile: "",
+                reply: "더 노력하도록 하겠습니다~"
+            },
+        ]
+    },
+    {
+        id: 4,
+        review: []
+    },
+    {
+        id: 5,
+        review: []
+    },
+    {
+        id: 6,
+        review: []
+    },
+    {
+        id: 7,
+        review: []
+    },
+];
+
+export default reviews;
--- pages/review/index.tsx
+++ pages/review/index.tsx
@@ -1,10 +1,99 @@
+import Image from "next/image";
+import reviews from "../../data/reviews";
 import LoginCheck from "../login_check";
 import styles from "../../styles/pages/Review.module.scss";
+import { useEffect, useState } from "react";
 
-export default function Review() {
+type review = {
+    id: number
+}
+type reviewTmp = {
+    name: string;
+    time: string;
+    comment: string;
+}
+
+export default function Review({id=1}: review) {
+    const [review, setReview] = useState<any>();
+    const [tmpReview, setTmpReview] = useState<reviewTmp>();
+
+    // 리뷰 입력 부분 출력
+    function printReviewInput() {
+        return (
+            <div className={styles.reviewInput}>
+                <hr />
+                <div className={styles.reviewListUserProfile}>
+                    <Image src={"/profile.webp"} alt={"profile"} width={50} height={50} priority className={styles.reviewListImage} />
+                    <div className={styles.reviewListUserInfo}>
+                        {review && (
+                            <>
+                                <p>{tmpReview!.name}</p>
+                                <p>{tmpReview!.time}</p>
+                                <p>{tmpReview!.comment}</p>
+                            </>
+                        )}
+                    </div>
+                </div>
+                <hr />
+                <input type="text" className={styles.reviewInputArea}/>
+            </div>
+        )
+    }
+
+    // 리뷰 리스트 출력
+    function printReviewList() {
+        let result = [];
+        for(let i=0; i<review.length; i++) {
+            result.push(
+                <div className={styles.reviewListItem} key={"review-item-" + i}>
+                    <hr />
+                    <div className={styles.reviewListUserProfile}>
+                        <Image src={"/profile.webp"} alt={"profile"} width={50} height={50} priority className={styles.reviewListImage} />
+                        <div className={styles.reviewListUserInfo}>
+                            {review && (
+                                <>
+                                    <p>{review[i].name}</p>
+                                    <p>{review[i].time}</p>
+                                    <p>{review[i].comment}</p>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className={styles.reviewListUserProfile}>
+                        <Image src={"/reply.webp"} alt={"reply"} width={50} height={50} priority className={`${styles.reviewListReplyImage} ${styles.reviewListImage}`} />
+                        {(review && review[i].reply) ? (
+                            <div className={styles.reviewListUserInfo}>
+                                <p>사장님</p>
+                                <p>{review[i].reply}</p>
+                            </div>
+                        ) : (
+                            <div className={styles.reviewListUserInfo}>
+                                <button onClick={() => setTmpReview(review[i])} className={styles.reviewListReplyBtn}>답글 달기</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )
+        }
+        return result;
+    }
+
+    useEffect(() => {
+        setReview(reviews[id - 1].review);
+    }, [])
+
     return LoginCheck() ? (
         <main>
-            <div>This is Review page.</div>
+            <section className={styles.review}>
+                <h1>매장 리뷰</h1>
+                <div>
+                    <div className={styles.reviewList}>
+                        {review && printReviewList()}
+                    </div>
+                    {tmpReview && (printReviewInput())}
+                </div>
+            </section>
         </main>
     ) : null;
 }
