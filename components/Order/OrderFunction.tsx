76c3143a62537c6301b384e3bb8c9ee9489f4a7b
JongGeun <dldvk9999@naver.com>
2022-12-19 16:26:57 +0900

    OrderFunction - React-Query error patch

--- components/Order/OrderFunction.tsx
+++ components/Order/OrderFunction.tsx
@@ -5,7 +5,10 @@ import { isAPI } from "../../states";
 // 주문 조회
 export function GetOrder(id: number) {
     if (isAPI) {
-        let result = useQuery("getOrder", () => APIGet("/api/store/" + id + "/order"), { refetchInterval: 1000 });
+        let result = useQuery(["getOrder", isAPI], () => APIGet("/api/store/" + id + "/order"), {
+            enabled: isAPI,
+            refetchInterval: 1000,
+        });
         return result;
     }
     return {};
